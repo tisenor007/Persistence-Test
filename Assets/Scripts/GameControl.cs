b0bdc0f4f0513f3eafe71630ef13b54464bf1b86
Trenton Isenor <ronesinotnert@gmail.com>
2021-09-27 15:59:15 -0300

    added character and stats

--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -1,13 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
 public class GameControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameControl control;
+
+    public int health;
+    public int shield;
+    public int XP;
+    public int kills;
+    public int ammo;
+    public float money;
+
+    void Awake()
     {
-        
+        if (control == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            control = this;
+        }
+        else if (control != this)
+        {
+            Destroy(gameObject);
+        }
     }
 
     // Update is called once per frame
@@ -15,4 +34,44 @@ public class GameControl : MonoBehaviour
     {
         
     }
+
+    public void Save()
+    {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/playerInfo.dat");
+        PlayerData data = new PlayerData();
+        data.health = health;
+        data.shield = shield;
+        data.ammo = ammo;
+        data.XP = XP;
+        data.kills = kills;
+        data.money = money;
+
+        bf.Serialize(file, data);
+        file.Close();
+    }
+    public void Load()
+    {
+        if (File.Exists(Application.persistentDataPath + "/playerInfo.dat"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/playerInfo.dat", FileMode.Open);
+
+            PlayerData data = (PlayerData)bf.Deserialize(file);
+            file.Close();
+
+
+        }
+    }
+
+}
+[Serializable]
+class PlayerData
+{
+    public int health;
+    public int shield;
+    public int XP;
+    public int kills;
+    public int ammo;
+    public float money;
 }
